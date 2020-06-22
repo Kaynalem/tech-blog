874ed3e71dca10f0f2e978550f36f410fc323aac
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-06-21 21:01:33 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
+let sequelize;
 
 if (process.env.JAWSDB_URL) {  
     sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -11,6 +12,7 @@ if (process.env.JAWSDB_URL) {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+    });
+}
 
 module.exports = sequelize;
