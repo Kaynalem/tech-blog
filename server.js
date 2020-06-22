631820de5cfdd26ca17cc19680c5634bd0be13e6
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-06-21 22:19:56 -0500

    update server

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
+
+const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
 const helpers = require('./utils/helpers');
-// set up Handlebars.js as your app's template engine of choice
-const app = express();
-const PORT = process.env.PORT || 3001;
-
+const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
-
 const session = require('express-session');
-require('dotenv').config();
+
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: process.env.SESS_SEC,
-    cookie: {
-        // automatically sign out user if idle on page more than 10 minutes
-        expires: 10 * 60 * 1000
-    },
+    cookie: {// automatically sign out user if idle on page more than 10 minutes
+        expires: 10 * 60 * 1000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -27,16 +22,19 @@ const sess = {
     })
 };
 
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
 // turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
