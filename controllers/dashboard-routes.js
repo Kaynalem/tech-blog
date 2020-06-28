419bbf57b93978b1acf86374f04b0a12bcefa240
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-06-28 14:32:06 -0500

    fix edit post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,8 +43,7 @@ router.get('/', withAuth, (req, res) => {
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
         where: {
-        // use the ID from the session
-        user_id: req.session.user_id
+            id: req.params.id
         },
         attributes: [
             'id',
